b391da6f2850eefc35734deeca2a12f69de919b0
Vedx66 <vedantgaikwad146@gmail.com>
2026-04-07 00:06:12 +0530

    fix launch server for hf 1.2

--- app.py
+++ app.py
@@ -635,5 +635,5 @@ if __name__ == "__main__":
     demo.queue().launch(
         server_name="0.0.0.0",
         server_port=7860,
-        share=not is_hf,   # ONLY true locally if needed
+        share= True,   # ONLY true locally if needed
     )
